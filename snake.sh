c1024dbf4689c5264383973fdfc33310de2487d7
Sara Ricossa <sara.ricossa@gmx.ch>
2024-10-06 17:00:32 +0200

    Clean up the code.

--- snake.sh
+++ snake.sh
@@ -10,12 +10,11 @@ stty -echo
 # at the end, flush, go to 0, show cursor and input
 trap "printf '\033[0;0H\033[J\0338\033[?25h'; stty echo; exit"\
         EXIT HUP INT TERM
-# get terminal dimensions
+# get terminal dimensions: W, H
 printf "\033[999;999H\033[6n"
 read -d R s
-IFS=\;
-read -r H W <<< ${s:2}
-IFS= #
+IFS=';'
+read -r H W <<< "${s:2}"
 
 # initialize RNG
 RANDOM=$(( $(date +%s) % 32768 ))
@@ -25,14 +24,16 @@ RANDOM=$(( $(date +%s) % 32768 ))
 
 
 highScore=0 # don't edit this, you cheater! >:(
-
 length=3 # snake length
+direction=left # direction of motion
+deltat=0.1 # 1/speed
+
 # We don't shift all the snake segments with every frame,
 # instead we keep track of the index corresponding to the head,
 # tail, and previous head:
-tail=0
-head=0
-prevHead=0
+curTail=0
+prevHead=1
+curHead=2
 
 gameOverFlag=0
 growingFlag=0 # whether the snake is growing
@@ -49,14 +50,7 @@ declare -a snakeY=( $(( H/2 )) $(( H/2 )) $(( H/2 )) )
 foodX=0 # food x
 foodY=0 # food y
 
-keystroke=$'\033[D' # pressed key. Can take values in:
-            # '\033[A' : up arrow
-            # '\033[B' : down arrow
-            # '\033[C' : right arrow
-            # '\033[D' : left arrow
-activeKeystroke=$keystroke # active key stroke
-                           # (may be different from pressed key if 
-                           # illicit move)
+keystroke= # pressed key. 
 
 
 ############ --- defining functions --- ############
@@ -82,50 +76,49 @@ place_food(){
 }
 place_food
 
-move_snake(){
-    # check if the current key press is an illicit move
-    # (snake cannot move backwards)
-    case $keystroke in
-        $'\033[A')
-            if [ $activeKeystroke != $'\033[B' ];
-                then activeKeystroke=$keystroke; fi;;
-        $'\033[B')
-            if [ $activeKeystroke != $'\033[A' ];
-                then activeKeystroke=$keystroke; fi;;
-        $'\033[C')
-            if [ $activeKeystroke != $'\033[D' ];
-                then activeKeystroke=$keystroke; fi;;
-        $'\033[D')
-            if [ $activeKeystroke != $'\033[C' ];
-                then activeKeystroke=$keystroke; fi;;
+parse_input(){
+    # parse keyboard input. Arrow keys take values in:
+    # \033[A : up arrow
+    # \033[B : down arrow
+    # \033[C : right arrow
+    # \033[D : left arrow
+    if [[ ! ${keystroke} =~ $'\033['[ABCD] ]]; then return; fi;
+    case ${keystroke:2} in
+        A) if [ $direction != down ]; then direction=up; fi;;
+        B) if [ $direction != up ]; then direction=down; fi;;
+        C) if [ $direction != left ]; then direction=right; fi;;
+        D) if [ $direction != right ]; then direction=left; fi;;
         *);;
     esac
+}
+
+move_snake(){
     # with each move, the frame index increments, and
     # the tail of the snake becomes the new head. In pseudo-code:
     # segments[current tail] = segments[current head] + increment.
-    case $activeKeystroke in
-        $'\033[A')
-            snakeY[head]=$(( snakeY[prevHead] - 1 ));
-            snakeX[head]=${snakeX[prevHead]};;
-        $'\033[B')
-            snakeY[head]=$(( snakeY[prevHead] + 1 ));
-            snakeX[head]=${snakeX[prevHead]};;
-        $'\033[C')
-            snakeX[head]=$(( snakeX[prevHead] + 1 ));
-            snakeY[head]=${snakeY[prevHead]};;
-        $'\033[D')
-            snakeX[head]=$(( snakeX[prevHead] - 1 ));
-            snakeY[head]=${snakeY[prevHead]};;
+    case $direction in
+        up)
+            snakeY[curHead]=$(( snakeY[prevHead] - 1 ));
+            snakeX[curHead]=${snakeX[prevHead]};;
+        down)
+            snakeY[curHead]=$(( snakeY[prevHead] + 1 ));
+            snakeX[curHead]=${snakeX[prevHead]};;
+        right)
+            snakeX[curHead]=$(( snakeX[prevHead] + 1 ));
+            snakeY[curHead]=${snakeY[prevHead]};;
+        left)
+            snakeX[curHead]=$(( snakeX[prevHead] - 1 ));
+            snakeY[curHead]=${snakeY[prevHead]};;
         *);;
     esac
 }
 
 check_collision(){
-    if [ ${snakeX[head]} -lt 0 ]\
-        || [ ${snakeY[head]} -lt 0 ]\
-        || [ ${snakeX[head]} -ge $W ]\
-        || [ ${snakeY[head]} -ge $H ]\
-        || [[ ${buffer[snakeY[head]*W + snakeX[head]]} -eq 1 ]]
+    if [ ${snakeX[curHead]} -lt 0 ]\
+        || [ ${snakeY[curHead]} -lt 0 ]\
+        || [ ${snakeX[curHead]} -ge $W ]\
+        || [ ${snakeY[curHead]} -ge $H ]\
+        || [[ ${buffer[snakeY[curHead]*W + snakeX[curHead]]} -eq 1 ]]
     then
         gameOverFlag=1
     fi
@@ -133,37 +126,39 @@ check_collision(){
 
 # to be executed every frame
 frame(){
-    if [ ${snakeX[head]} -eq $foodX ] && [ ${snakeY[head]} -eq $foodY ]
+    if [ ${snakeX[curHead]} -eq $foodX ]\
+        && [ ${snakeY[curHead]} -eq $foodY ]
     then
         growingFlag=1
         place_food
     fi
 
-    if [ $growingFlag -eq 1 ] && [ $tail -eq 0 ]; then
+    if [ $growingFlag -eq 1 ] && [ $curTail -eq 0 ]; then
     # we only increment the length of the snake when the frame index hits
     # 0 to avoid issues.
         ((length++))
         growingFlag=0
     else
         # erase the old tail
-        output ${snakeX[tail]} ${snakeY[tail]} ' '
-        buffer[snakeY[tail]*W + snakeX[tail]]=0
+        output ${snakeX[curTail]} ${snakeY[curTail]} ' '
+        buffer[snakeY[curTail]*W + snakeX[curTail]]=0
         # increment the indices
-        ((tail++))
-        ((tail %= length))
+        ((curTail++))
+        ((curTail %= length))
     fi
 
     # define the new positions for head and previous head
-    prevHead=$(( (tail + length - 2) % length ))
-    head=$(( (prevHead + 1) % length ))
+    prevHead=$(( (curTail + length - 2) % length ))
+    curHead=$(( (prevHead + 1) % length ))
 
+    parse_input
     move_snake
     check_collision
 
     # print the new head    
     output ${snakeX[prevHead]} ${snakeY[prevHead]} o
-    output ${snakeX[head]} ${snakeY[head]} @
-    buffer[snakeY[head]*W + snakeX[head]]=1
+    output ${snakeX[curHead]} ${snakeY[curHead]} @
+    buffer[snakeY[curHead]*W + snakeX[curHead]]=1
 }
 
 
@@ -180,13 +175,13 @@ do
         output $((W/2 - 6)) $((H/2-1)) " GAME OVER!! "
         output $((W/2 - 6)) $((H/2)) " Score: $(( length - 3 )) "
         output $((W/2 - 6)) $((H/2+1)) " High-score: $highScore "
-        read -N 1 _
+        read -n1 _
         exit
     else
         frame
-        read -t 0.1 -N 3 keystroke
+        read -t $deltat -N3 keystroke
         # implement a pause button (space bar)
-        if [ "$keystroke" = ' ' ]; then read -N 1 _; fi
+        if [ "$keystroke" = ' ' ]; then read -n1 _; fi
     fi
 done
 
