ffbe8621eef3d0782ff95c17e99399933f50e7d8
Sara Ricossa <sara.ricossa@gmx.ch>
2025-03-27 17:37:39 +0100

    Reset high score to 0 for new game sessions

--- snake.sh
+++ snake.sh
@@ -23,7 +23,7 @@ RANDOM=$(( $(date +%s) % 32768 ))
 ############ --- program variables --- ############
 
 
-highScore=4 # don't edit this, you cheater! >:(
+highScore=0 # don't edit this, you cheater! >:(
 length=3 # snake length
 direction=left # direction of motion
 deltat=0.1 # 1/speed
